4159c74142ff892def039055d854647b17fd7cbc
Milan Jalic <milanjalic@gmail.com>
2018-02-26 23:21:31 +0100

    created Car class

---
+++ src/app/cars/car.ts
@@ -0,0 +1,15 @@
+export class Car {
+  public image: string = null;
+  public speed: number;
+  public description: string = null;
+  public name: string = null;
+  public id: number;
+
+  constructor(image: string, speed: number, description: string, name: string, id: number) {
+    this.image = image;
+    this.speed = speed;
+    this.description = description;
+    this.name = name;
+    this.id = id;
+  }
+}
